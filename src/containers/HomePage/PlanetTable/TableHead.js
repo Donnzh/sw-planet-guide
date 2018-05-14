c871c97bb7d5be077e2d765ea46716da6d364047
Donn Zhao <z.donn@emondo.com.au>
2018-05-14 13:49:32 +1000

    add table header

---
+++ src/containers/HomePage/PlanetTable/TableHead.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  TableCell,
+  TableHead,
+  TableRow,
+  TableSortLabel,
+} from 'material-ui/Table';
+import Tooltip from 'material-ui/Tooltip';
+
+const columnData = [
+  {id: 'name', numeric: false, disablePadding: true, label: 'Planet'},
+  {id: 'population', numeric: true, disablePadding: false, label: 'Population'},
+  {id: 'terrain', numeric: true, disablePadding: false, label: 'Terrain'},
+  {id: 'climate', numeric: true, disablePadding: false, label: 'Climate'},
+  {id: 'gravity', numeric: true, disablePadding: false, label: 'Gravity'},
+];
+
+export default class EnhancedTableHead extends React.Component {
+  createSortHandler = property => event => {
+    this.props.onRequestSort(event, property);
+  };
+
+  render () {
+    const {
+      order,
+      orderBy
+    } = this.props;
+    return (
+      <TableHead>
+        <TableRow>
+          <TableCell padding="checkbox">
+          </TableCell>
+          {columnData.map((column,i) => {
+            return (
+              <TableCell
+                key={i}
+                numeric={column.numeric} //If true, content will align to the right.
+                padding={column.disablePadding ? 'none' : 'default'}
+                sortDirection={orderBy === column.id ? order : false} //Set aria-sort direction.
+              >
+                <Tooltip
+                  title="Sort"
+                  placement={column.numeric ? 'bottom-end' : 'bottom-start'}
+                  enterDelay={300}>
+                  <TableSortLabel
+                    active={orderBy === column.id}
+                    direction={order}
+                    onClick={this.createSortHandler(column.id)}>
+                    {column.label}
+                  </TableSortLabel>
+                </Tooltip>
+              </TableCell>
+            );
+          })}
+        </TableRow>
+      </TableHead>
+    );
+  }
+}
+
+EnhancedTableHead.propTypes = {
+  onRequestSort: PropTypes.func.isRequired,
+  order: PropTypes.string.isRequired,
+  orderBy: PropTypes.string.isRequired,
+  rowCount: PropTypes.number.isRequired,
+};
